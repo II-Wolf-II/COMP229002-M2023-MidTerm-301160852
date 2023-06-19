2d40da91d1ec59fac4c54f5ec9bd78917ad1c7e9
Aaron Ainsworth <105944277+II-Wolf-II@users.noreply.github.com>
2023-06-19 19:57:19 -0400

    Updated controllers\Car.js
    Completed displayAddpage function
    Completed processAddPage function
    Completed displayEditPage function
    Completed processEditPage function
    Completed performDelete function

--- controllers/car.js
+++ controllers/car.js
@@ -29,7 +29,7 @@ module.exports.details = async (req, res, next) => {
 
         res.render('cars/details', {
             title: 'Car Details', 
-            car: carToShow
+            cars: carToShow
         })
     } catch (error) {
         console.log(error);
@@ -40,7 +40,17 @@ module.exports.details = async (req, res, next) => {
 // Renders the Add form using the add_edit.ejs template
 module.exports.displayAddPage = (req, res, next) => {
     
-    // ADD YOUR CODE HERE        
+    // ADD YOUR CODE HERE 
+    try{
+    let newcar = CarModel()
+    return res.render('cars/add_edit', {
+            title:"Add New Car",
+            car: newcar,
+            userName: req.user ? req.user.username : ''
+        });  
+    } catch(error){
+        console.log(error)
+    }           
 
 }
 
@@ -48,26 +58,107 @@ module.exports.displayAddPage = (req, res, next) => {
 module.exports.processAddPage = (req, res, next) => {
 
     // ADD YOUR CODE HERE
+    let newcar = new CarModel({
+        _id: req.body.id, 
+        make: req.body.make,
+        model: req.body.model,
+        year: req.body.year,
+        kilometers: req.body.kilometers,
+        doors: req.body.doors,
+        seats: req.body.seats,
+        color: req.body.color,
+        price: req.body.price,
+    });
+CarModel.create(newcar, (err, item) =>{
+    if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            // refresh the book list
+            console.log(item);
+            res.redirect('/cars/list');
+        }
+});
+    
+    
 
 }
 
 // Gets a car by id and renders the Edit form using the add_edit.ejs template
 module.exports.displayEditPage = (req, res, next) => {
-    
-    // ADD YOUR CODE HERE
+    let id = req.params.id;
 
+    CarModel.findById(id, (err, carToShow) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //show the edit view
+            res.render('cars/add_edit', {
+                title: 'Edit Car', 
+                car: carToShow
+            })
+        }
+    });
+    
+    
 }
+
 
 // Processes the data submitted from the Edit form to update a car
 module.exports.processEditPage = (req, res, next) => {
     
     // ADD YOUR CODE HERE
-    
+    let id = req.params.id;
+
+    let updatedcar = CarModel({
+        _id: id,
+        make: req.body.make,
+        model: req.body.model,
+        year: req.body.year,
+        kilometers: req.body.kilometers,
+        doors: req.body.doors,
+        seats: req.body.seats,
+        color: req.body.color,
+        price: req.body.price,
+    });
+    CarModel.update({_id: id}, updatedcar, (err) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+
+            res.redirect('/cars/list');
+        }
+    });
+
 }
 
 // Deletes a car based on its id.
 module.exports.performDelete = (req, res, next) => {
     
     // ADD YOUR CODE HERE
+    let id = req.params.id;
+    CarModel.remove({_id: id}, (err) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            // refresh the book list
+            res.redirect('/cars/list');
+        }
+    });
 
 }
